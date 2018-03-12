677c9cc1c3d6f1f5ee718c7bc64b88cc14de32df
AnthonyLam7 <anthonylam7@gmail.com>
2018-03-12 15:42:09 -0400

    implemented main categorizing function + added defaults setting

--- categorize.py
+++ categorize.py
@@ -58,6 +58,23 @@ import argparse
 import configparser
 
 
+DEFAULT_CONFIG = {
+    "EXTENSION": {
+        "audio": {".wav", ".mp3", ".mid", ".midi", ".mpa", ".wma", ".ogg"},
+        "documents": {".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt", ".odt", ".pdf", ".txt"},
+        "compressed": {".zip", ".7z", ".gz", ".arj", ".pkg", ".deb", ".rpm", ".z", ".zip"},
+        "data": {".csv", ".dat", ".db", ".dbf", ".log", ".mdb", ".sql", ".sav", ".xml"},
+        "images": {".ai", ".bmp", ".png", ".jpeg", ".jpg", ".gif", ".ico", ".ps", ".psd", ".svg", ".tif", ".tiff"},
+        "videos": {".3g2", ".3gp", ".avi", ".flv", ".h264", ".m4v", ".mkv", ".mov", ".mp4", ".mpg", ".mpeg", ".swf",
+                   ".rm", ".vob", ".wmv"}
+    },
+    "DIRECTORY":{
+        "media": {"images", "videos", "audio"}
+    }
+
+}
+
+
 # Setup argparser
 parser = argparse.ArgumentParser(
     description="extension based file grouping tool",
@@ -121,18 +138,18 @@ ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
 
-def parse_config_dict(config):
+def parse_config_dict(config, **kwargs):
     """
     Takes a config dictionary where values are sequ of strings and creates a new dictionary where each string becomes
     the key and the original key becomes the new value
-    :param config:
-    :return:
+    :param config: dict {"dirnam" : set {extensions}}
+    :return: dict {"extensions": "dirnam"}
     """
-    if not isinstance(config, (dict)):
+    if not isinstance(config, (dict,)):
         raise TypeError("Argument should be of type dict!")
     lut = {}
     for k, v in config.items():
-        if not hasattr(v, "__iter__") or isinstance(v, (iter)):
+        if not hasattr(v, "__iter__") or isinstance(v, (str,)):
             raise TypeError("Values should be a seq of strings!")
         for item in v:
             lut[item] = k
@@ -145,16 +162,17 @@ def create_file_table(target, ext_map, **kwargs):
     :param target: path to target dir
     :param ext_map: dict {dirname: set {extensions,}
     :param kwargs:
-    :return:
+    :return: dict {"dirname": set {filenames}
     '''
     table = {}
-    pattern = ".*(\\..*)"
+    pattern = "^.*(\\..*)$"
     regex = re.compile(pattern)
     try:
         for basename in os.listdir(target):
             if os.path.isfile(os.path.join(target, basename)):
                 match = regex.match(basename)
-                ext = match.group()
+                ext = match.group(1)
+                logger.debug("Matched extension {}".format(ext))
                 dir_name_key = ext_map[ext]
                 if not table.get(dir_name_key):
                     table[dir_name_key] = set()
@@ -163,6 +181,34 @@ def create_file_table(target, ext_map, **kwargs):
         logger.error("File not found.")
         raise FileNotFoundError("Please provide a valid path to a directory!")
     return table
+
+
+def categorize(src, destination, config_dict=DEFAULT_CONFIG, **kwargs):
+    dir_ext_lut = config_dict["EXTENSION"]
+    dir_subdir_lut = config_dict["DIRECTORY"]
+    ext_dir_lut = parse_config_dict(dir_ext_lut)
+    subdir_dir_lut = parse_config_dict(dir_subdir_lut)
+    file_table = create_file_table(src, ext_dir_lut)
+
+    for dir, files in file_table.items():
+        path = []
+        path.append(dir)
+        parent_dir = subdir_dir_lut.get(dir, None)
+        while parent_dir:
+            path.append(parent_dir)
+            parent_dir = subdir_dir_lut.get(parent_dir, None)
+        path.reverse()
+        path = os.path.join(destination, *path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        for file in files:
+            file_loc = os.path.join(src, file)
+            dest_loc = os.path.join(path, file)
+            if os.path.exists(dest_loc):
+                logger.warning("File at {} naming conflict with file at {}. Skipping.".format(file_loc, dest_loc))
+                continue
+            os.rename(file_loc, dest_loc)
+
 
 
 if __name__ == "__main__":
